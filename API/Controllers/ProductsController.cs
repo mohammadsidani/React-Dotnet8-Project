bdb2778e37c74474792dfc6cc1ca39ad5bac5c95
Mohammad Sidani <mohammadsidani13@gmail.com>
2024-02-20 23:08:13 +0200

    React Router and Error Handling

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -3,29 +3,28 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+public class ProductsController : BaseApiController
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    private readonly StoreContext _context;
+    public ProductsController(StoreContext context)
     {
-        private readonly StoreContext _context;
+        _context = context;
+    }
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+        return await _context.Products.ToListAsync();
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        
+        var product = await _context.Products.FindAsync(id);
 
-        public ProductsController(StoreContext context)
-        {   
-            _context = context;
-        }
-        [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
-        {
-            return await _context.Products.ToListAsync();
+        if (product == null) return NotFound();
 
-        }
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
-        {
-            return await _context.Products.FindAsync(id);
-        }
-
+        return product;
     }
 }
--- frontend/src/features/catalog/Catalog.tsx
+++ frontend/src/features/catalog/Catalog.tsx
@@ -1,20 +1,30 @@
-import { Product } from "../../app/models/product";
-import ProductList from "./ProductList";
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import { Product } from '../../app/models/product';
+import ProductList from './ProductList';
+import agent from '../../app/api/agent';
+import LoadingComponent from '../../app/layout/LoadingComponent';
 
-export default function Catalog(){
+export default function Catalog() {
+    const [loading, setLoading] = useState(true);
     const [products, setProducts] = useState<Product[]>([]);
 
-  useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-    .then(response => response.json())
-    .then(data => setProducts(data))
-  }, [])
+    useEffect(() => {
+        agent.Catalog.list()
+            .then(products => {
+                setProducts(products)
+            })
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false));
+    }, [])
 
 
-    return(
+    
+
+    if (loading) return <LoadingComponent message='Loading products...' />
+
+    return (
         <>
-        <ProductList products={products} />
+            <ProductList products={products} />
         </>
     )
 }
